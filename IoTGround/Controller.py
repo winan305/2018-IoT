c7ef9f088d7f041aed6ba9a75fde28ced14a1414
DESKTOP-L4LPR49\Administrator <winan305>
2018-04-20 17:58:24 +0900

    IoT Team Project Source Code - 2018-04-20

---
+++ IoTGround/Controller.py
@@ -0,0 +1,108 @@
+import RPi.GPIO as GPIO
+import time
+import spidev
+
+class Controller :
+    '''
+    컨트롤러 클래스
+    조도센서 8개, 서보모터 4개
+    '''
+    TARGET_NUMBER = 4
+    FLAG_TARGET_UP, FLAG_TARGET_DOWN = 0 , 1
+    TARGET_STATE = [False] * TARGET_NUMBER
+    TARGET_UP_ANGLE, TARGET_DOWN_ANGLE = 1, 8
+    # GPIO 5, 6, 13, 19 사용
+    motorPins = [5, 6, 13, 19]
+    pmws = []
+
+    LIGHT_SENSOR_NUMBER = 8
+    light_channels = [i for i in range(LIGHT_SENSOR_NUMBER)]
+    AVG_LIGHT_VALUE = [-9876543210] * LIGHT_SENSOR_NUMBER
+
+    def __init__(self):
+        '''
+        생성자 함수.
+        여기서 핀모드를 설정하든 뭘 하든 하면 되겠지?
+        '''
+        print("Controller Class Object init")
+        # 여기서부터 모터
+
+        GPIO.setmode(GPIO.BCM)
+
+        for motorPin in self.motorPins :
+            GPIO.setup(motorPin, GPIO.OUT)
+            p = GPIO.PWM(motorPin, 50)
+            p.start(0)
+            self.pmws.append(p)
+
+        # 여기서부터 조도센서 spi모듈 사용을 위해 초기화
+        self.spi = spidev.SpiDev()
+        self.spi.open(0, 0)
+        self.spi.max_speed_hz = 61000
+
+    def readChannel(self, channel):
+        adc = self.spi.xfer2([1, (8 + channel) << 4, 0])
+        adc_out = ((adc[1] & 3) << 8) + adc[2]
+        return adc_out
+
+    def getLigthValue(self, channel) :
+        '''
+        조도센서로부터 값을 얻는 함수
+        :param wait: 반복간 얼마나 기다리면서 측정할 것인가? 기본 0초
+        :param repeat: 얼마나 반복할 것인가? 기본 1회
+        :return: 조도센서 값 리턴
+        '''
+        light_level = self.readChannel(channel)
+        return light_level
+
+    def controllTarget(self, number, flag) :
+        '''
+        서보모터 번호와 각도를 입력받아서 돌림(과녁을 눕히고 세움)
+        :param number: 서보모터(타겟) 번호(1번 과녁, 2번과녁 이렇게 되겠지.)
+        :param flag: 눕힐래 세울래? 90도냐 -90도냐는 고정이고.
+        :return:
+        '''
+        if flag == self.FLAG_TARGET_UP :
+            self.pmws[number].ChangeDutyCycle(self.TARGET_UP_ANGLE)
+            pass
+
+        elif flag == self.FLAG_TARGET_DOWN :
+            self.pmws[number].ChangeDutyCycle(self.TARGET_DOWN_ANGLE)
+            pass
+
+    def getLightSensorState(self, channel) :
+        light_value = self.getLigthValue(channel)
+        return light_value > self.AVG_LIGHT_VALUE + 50
+
+    def initSensorState(self) :
+        '''
+        센서 초기상태를 설정하는 함수. 그래봐야 조도센서 뿐이겠지.
+        현재 장소의 평균 조도센서 값을 저장함
+        why?
+        장소, 계절, 조명상태마다 조도센서 값이 달라짐
+        달라지는 그 값을 기준으로 레이저를 받았을 때 값차이가 피격의 기준이 되야함
+        그래야 어느곳에서든 할 수 있다.
+
+        how?
+        처음 게임이 실행되면 이 함수를 호출함
+        조도센서로부터 값을 5회정도 받아서 평균내면 되지 않을까?
+        '''
+
+        # 모든 표적지를 세움
+        for pmw in self.pmws :
+            pmw.ChangeDutyCycle(self.TARGET_UP_ANGLE)
+
+        for _ in range(5) :
+            for light_channel in self.light_channels :
+                self.AVG_LIGHT_VALUE[light_channel] += self.getLigthValue(light_channel)
+            time.sleep(0.5)
+
+        for i in light_channel :
+            self.AVG_LIGHT_VALUE[i] = int(self.AVG_LIGHT_VALUE[i] / 5)
+
+        # 모든 표적지를 눕힘
+        for pmw in self.pmws :
+            pmw.ChangeDutyCycle(self.TARGET_DOWN_ANGLE)
+
+        print("Call initSensorState(), result =", self.AVG_LIGHT_VALUE)
+
---
+++ IoTGround/Game.py
@@ -0,0 +1,95 @@
+import IoTGround.Controller as Controller
+import random
+
+class Game:
+    '''
+    게임진행 메인 클래스
+    1. 게임 요청을 받고
+    2. 게임 준비를 하고
+    3. 게임 진행을 하고
+    4. 게임 결과를 저장
+    5. 게임 내용 초기화
+    위 과정 반복.
+    waitRequest() -> ready2Play() -> play() -> saveResult() -> reset() -> waitRequest() -> ...
+    위 과정을 run() 에서 실행
+    '''
+
+    NUM_OF_TARGETS = 4
+    REQ_TARGET_UP, REQ_TARGET_DOWN = 0, 1
+    def __init__(self):
+        '''
+        파이에 전원 꽂으면 Main 객체가 생성되도록 하고
+        컨트롤러 객체 생성해서 컨트롤러 핀 모드 초기화
+        그러고나서 게임에 필요한거 초기화하고 run 함수 부르면 될듯
+        '''
+        print("Game Class Object init")
+        self.controller = Controller.Controller()
+        self.controller.initSensorState()
+        pass
+
+    def waitRequest(self):
+        '''
+        안드로이드 앱으로부터 요청이 올 때 까지 대기.
+        '''
+        print("Call waitRequest()")
+        pass
+
+    def ready2play(self):
+        '''
+        앱으로 부터 게임모드, 인원수 등을 받았겠지
+        그 값들 세팅
+        컨트롤러에서 initSensorState 함수 호출
+        왜 여기서 이 함수를 호출하냐?
+        게임 진행 전 마다 세팅해줘야 그 순간순간 조도값에 맞출 수 있겠지?
+        '''
+        print("Call ready2play()")
+        pass
+
+    def play(self):
+        '''
+        게임 진행 하는 함수지 뭐.
+        게임모드, 인원수 맞춰서 표적을 세워야 함
+        아마 멀티 쓰레딩이 필요할 듯 싶은데. 흠..
+        고민좀 해봐야할듯
+        '''
+        print("Call play()")
+        target = random.random(self.NUM_OF_TARGETS)
+        self.controller.controllTarget(target, self.REQ_TARGET_UP)
+
+        # 조도센서의 상태가 True 이면 피격당한것
+        while not self.controller.getLightSensorState(target) :
+            pass
+        self.controller.controllTarget(target, self.REQ_TARGET_DOWN)
+
+        pass
+
+    def saveResult(self):
+        '''
+        게임 끝나면 결과가 저장되어 있겠지?
+        그 값들 데이터베이스에 저장하자.
+        '''
+        print("Call saveResult()")
+        pass
+
+    def reset(self):
+        '''
+        게임 진행하면서 저장된 값들이 있을 것임.
+        그 값들을 초기화 해서 다음 게임에 지장없게.
+        '''
+        print("Call reset()")
+        pass
+
+    def run(self):
+        '''
+        런~
+        '''
+        print("Call run()")
+        self.waitRequest()
+        self.ready2play()
+        self.play()
+        self.saveResult()
+        self.reset()
+        pass
+
+game = Game()
+game.run()
