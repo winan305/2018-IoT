bd10173105257b3a8eaf7f0736963d7dc39b2ce8
DESKTOP-L4LPR49\Administrator <winan305>
2018-05-13 19:32:22 +0900

    2018-05-13 IoT LED Smart Home - 업데이트 내용 : 주석달았음.

--- LEDSmartHome/LEDController.py
+++ LEDSmartHome/LEDController.py
@@ -10,8 +10,10 @@ import RPi.GPIO as gpio
 
 # 초록, 노랑, 빨강 LED GPIO 핀 번호
 led_pin = [21, 20, 16]
+
 # LED 상태 플래그
 LED_FLAG_GREEN, LED_FLAG_YELLOW, LED_FLAG_RED, LED_FLAG_ALL = 0, 1, 2, 3
+# LED 플래그들에 대한 출력 문구
 LED_FLAGS = ["GREEN LED ON", "YELLOW LED ON", "RED LED ON", "ALL LED ON"]
 
 # mqtt 클라이언트 객체 생성
@@ -19,12 +21,13 @@ mqttc = mqtt.Client()
 
 # mqtt서버 접속 시 호출되는 콜백함수
 def on_connect(client, userdata, flags, rc) :
-    # 전등 컨트롤러가 mqtt 서버에 연결되었음을 출력한다.
+    # LED 컨트롤러가 mqtt 서버에 연결되었음을 출력한다.
     print("LED Controller Connected.")
 
-    # 전등 제어값을 subscribe 한다.
+    # LED 제어값을 subscribe 한다.
     client.subscribe("control/led")
-    
+
+    # LED 핀들의 gpio를 세팅한다.
     gpio.setmode(gpio.BCM)
     for pin in led_pin :
         gpio.setup(pin, gpio.OUT)
@@ -34,7 +37,7 @@ def on_message(client, userdata, msg) :
     # 데이터는 바이너리 데이터이다.
     # 아스키 데이터로 디코드하여 저장한다.
     led_flag = int(msg.payload.decode('ascii'))
-
+    # LED를 켜고 끄는 함수에 led 플래그를 전달한다.
     turnOnOffLED(led_flag)
 
 # LED 플래그로부터 LED를 켜고 끄는 함수다.
@@ -67,6 +70,7 @@ def start_mqtt(host="localhost", port=1883, keepalive=60) :
     # 루프를 실행한다.
     mqttc.loop_forever()
 
+# 키보드 인터럽트가 발생하면 gpio핀을 클린업 한다.
 try :
     # mqtt 클라이언트를 시작하는 함수를 호출한다.
     start_mqtt()
